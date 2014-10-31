4a8a10d15f641ba1d8ef6132175e6106e49e6b1e
Nuria Gonzalo <alu0100598155@ull.edu.es>
2014-10-31 00:48:20 +0000

    Modificando el fichero Exam.rb de la lib para las pruebas

--- lib/Exam/Exam.rb
+++ lib/Exam/Exam.rb
@@ -1,16 +1,34 @@
 # coding: utf-8
-require "Question"
+require 'Question'
 
 class Exam
 
-	attr_accessor :head, :tail
+	attr_accesor :head, :tail
 
-	Node = Struct.new :value, :next
+	Node = struct.new :value, :next
 	
 	def initialize (o)
 		raise TypeError, "Se esperaba que se pasara una pregunta como parámetro" unless o.instance_of? (Question)
 		@head = Node.new(o, nil)
 		@tail = @head
 	end
-	
+
+	 def pop		# modulo para poder sacar la pregunta 
+	    hd = @head		#hd =head
+	    @head = @head.next
+	    hd.next = nil
+	    hd.value
+	  end
+
+	  def <<(0)    
+	     raise TypeError, "Esperando la pregunta para su inserción" unless o.instance_of? (Pregunta) 
+	     @tail.next = Nodo.new(o, nil)
+	     @tail = @tail.next    
+	     @tail.value
+	  end
+
+	end
+
 end
+
+
