d4cc42c41c1480189ff1226a50395b9ceb9b9e87
Simón Fuertes <simonfuesan@gmail.com>
2014-10-31 00:26:02 +0000

    Implementado código que cumple los requisitos de la primera espectativa propuesta "Debe existir un Nodo de la lista con sus datos y su siguiente"

--- spec/Exam_spec.rb
+++ spec/Exam_spec.rb
@@ -7,33 +7,27 @@ class Question
 			@q = Question.new(:qt => '1+1=', :r1 => 2, :wrong => [3,5,7])
 		end
 						
-		context "Al realizar la pregunta" do
-			it "Debe existir la pregunta" do
+		context "Simple_Selection" do
+			it ":Debe existir una pregunta" do
 				expect(@q.qt)== '1+1='
 			end
-			it "Debe contener opciones para la respuesta" do
+			it ":Deben existir opciones de respuesta" do
 				expect(@q.r1)== 2
 				expect(@q.wrong)== [3,5,7]
 			end
 				#expect(@q).to be_instance_of(LPP_T_02::Question)			
 				#Esta línea comprueba que el objeto q sea un objeto instanciado de nuestra clase Question 
-		end
-						
-		context "Se debe poder invocar" do
-			it "un método para obtener la pregunta" do
+			it ":Se debe invocar un método para obtener la pregunta" do
 				expect(@q).to respond_to :qt
 			end
-			it "un método para obtener las opciones de respuesta" do
+			it ":Se debe invocar a un método para obtener las opciones de respuesta" do
 				expect(@q).to respond_to :r1
 				expect(@q).to respond_to :wrong
+			end		
+			it ":Se deben mostrar por la pantalla la pregunta y las opciones" do
+				expect(@q).to respond_to :to_s
 			end
 		end
-			
-			
-		it "Debe mostrarse por la pantalla la pregunta y las opciones" do
-			expect(@q).to respond_to :to_s
-		end
-					
 	end
 end
 	
@@ -41,9 +35,16 @@ class Exam
 	describe Exam do
 		
 		before :each do
-			@q = Question.new(:qt => '2+2=', :r1 => 4, :wrong => [3,5,7])
-			@n = Exam::Node.new (@q, nil)
+			@q = Question.new(:qt => '2+2=', :r1 => 4, :wrong => [2,5,8])
+			@n = Node.new (@q)
 			@e = Exam.new(@q)
+		end
+		
+		context "Node" do
+			it ":Debe existir un Nodo de la lista con sus datos y su siguiente" do
+				expect(@n.value)==@q
+				expect(@n.next)==nil
+			end
 		end
 		
 	end
